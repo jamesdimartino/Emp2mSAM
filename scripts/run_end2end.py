0fba2e1a91bc5c140deecedabcb088429b04b85a
jamesdimartino <55374208+jamesdimartino@users.noreply.github.com>
2024-07-26 10:50:41 -0700

    Update run_end2end.py

--- scripts/run_end2end.py
+++ scripts/run_end2end.py
@@ -1,4 +1,4 @@
-gimport os
+import os
 import argparse
 
 def parse_args():
@@ -34,7 +34,7 @@ def run_merge(input_dir, output_dir):
     os.system(f"python merge_instances.py {input_dir} {output_dir}")
 
 def run_stitch(input_dir, output_tiff_path, original_shape, tile_shape):
-    os.system(f"python stitch_save.py {input_dir} {output_tiff_path} --original_shape {original_shape[0]} {original_shape[1]} {original_shape[2]} --tile_shape {tile_shape[0]} {tile_shape[1]} {tile_shape[2]}")
+    os.system(f"python stitch_save.py {input_dir} {output_tiff_path} --original_shape {original_shape[0]} {original_shape[1]} {original_shape[2]} --tile_shape {original_shape[0]} {tile_shape[1]} {tile_shape[2]}")
 
 def main():
     args = parse_args()
